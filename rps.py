6ff3a1c7a5f12331d86f55288e78eb52e9e0c751
Aaron Smith <smitham50@gmail.com>
2019-11-25 10:14:36 -0500

    better game flow, need to win 3

--- rps.py
+++ rps.py
@@ -4,22 +4,52 @@ print('...rock...')
 print('...paper...')
 print('...scissors...')
 
-player1 = input("Enter Player 1's choice: ")
+player_score = 0
+computer_score = 0
 
+game_over = False
+
+#computer choices
 choices = ["rock", "paper", "scissors"]
 
-move = random.choice(choices)
+#player move
+player = input("Enter your choice: ")
 
-print(f"The computer played: {move}")
+print("")
+
+#computer move
+computer = random.choice(choices)
+print(f"The computer played: {computer}")
+
+while not game_over:
+    if player == computer:
+        print("Draw!")
+    elif player == "rock" and computer == "scissors":
+        print("You win!")
+        player_score += 1
+    elif player == "paper" and computer == "rock":
+        print("You win!")
+        player_score += 1
+    elif player == "scissors" and computer == "paper":
+        print("You win!")
+        player_score += 1
+    else:
+        print("The computer wins!")
+        computer_score += 1
+    print(f"Your score is: {player_score} || The computer's score is: {computer_score}")
+    if player_score == 3 or computer_score == 3:
+        if player_score == 3:
+            print("You win!\n")
+        else:
+            print("You lose!\n")
+        game_over = True
+    else:
+        print('...rock...')
+        print('...paper...')
+        print('...scissors...\n')
+        player = input("Enter your choice: ")
+        print("")
+        computer = random.choice(choices)
+        print(f"The computer played: {computer}\n")
 
 
-if player1 == move:
-    print("Draw!")
-elif player1 == "rock" and move == "scissors":
-    print("Player 1 Wins!")
-elif player1 == "paper" and move == "rock":
-    print("Player 1 Wins!")
-elif player1 == "scissors" and move == "paper":
-    print("Player 1 Wins!")
-else:
-    print("The computer Wins!")
